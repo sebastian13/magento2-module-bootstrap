43f0740fd0a37c2d1c76dfc7c3175941e8d8715d
Sebastian Plocek <sebastian@plocek.at>
2019-04-09 23:32:52 +0200

    updated bundle.js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
   paths: {
     'jquery.bootstrap': [
       'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min',
-      'Sebastian13_TwitterBootstrapJs/bootstrap'
+      'Sebastian13_TwitterBootstrapJs/bootstrap.bundle'
     ]
   },
   shim: {
